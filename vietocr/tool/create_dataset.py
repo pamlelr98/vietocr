3f8d7537cf44e184b3adce633dc1de162635c5c7
Pham Le <pamlelr98@gmail.com>
2025-06-29 15:48:30 +0700

    a

--- vietocr/tool/create_dataset.py
+++ vietocr/tool/create_dataset.py
@@ -47,7 +47,7 @@ def createDataset(outputPath, root_dir, annotation_path):
         annotations = [l.strip().split("\t") for l in lines]
 
     nSamples = len(annotations)
-    env = lmdb.open(outputPath, map_size=20 * 1024 * 1024 * 1024)
+    env = lmdb.open(outputPath, map_size=1099511627776)  # 1TB
     cache = {}
     cnt = 0
     error = 0
